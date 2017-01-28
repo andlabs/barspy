3d826e00c61120c414d7cfc79ed41d639f67c870
Pietro Gagliardi <pietro10@mac.com>
2017-01-28 00:15:54 -0500

    Revert "More attempts. I guess playing with the treeview isn't going to work, and that wasting memory is..."
    This reverts commit a114166e4113b0107669586cfeb387f538edfb90.

--- enum.cpp
+++ enum.cpp
@@ -3,48 +3,42 @@
 
 // whoops, EnumChildWindows() is already recursive
 // TODO can TVM_GETNEXTITEM return an error other than "no more items"? and isn't root NULL?
-static HTREEITEM locate(HWND tree, LPARAM lParam, HTREEITEM cur)
+static HTREEITEM windowParentNode(HWND tree, HWND hwnd)
 {
-	HTREEITEM child;
+	DWORD style;
+	HWND parent;
+	HTREEITEM nparent;
+	HTREEITEM cur;
 	TVITEMEXW tvi;
 
+	// TODO replace this with the explicit call
+	style = GetWindowStyle(hwnd);
+	// this function only works on child windows
+	// all non-child windows are at the top level of our tree, so...
+	if ((style & WS_CHILD) == 0)
+		return TVI_ROOT;
+
+	parent = GetAncestor(hwnd, GA_PARENT);
+	nparent = windowParentNode(tree, parent);
+	if (nparent == TVI_ROOT)
+		nparent = (HTREEITEM) SendMessageW(tree, TVM_GETNEXTITEM, TVGN_ROOT, 0);
+	cur = (HTREEITEM) SendMessageW(tree, TVM_GETNEXTITEM, TVGN_CHILD, (LPARAM) nparent);
 	while (cur != NULL) {
 		ZeroMemory(&tvi, sizeof (TVITEMEXW));
 		tvi.mask = TVIF_PARAM;
 		tvi.hItem = cur;
 		if (SendMessageW(tree, TVM_GETITEMW, 0, (LPARAM) (&tvi)) == (LRESULT) FALSE)
-			panic(L"error getting info of tree item searching for items by lParam: %I32d", GetLastError());
-		if (tvi.lParam == lParam)
+			panic(L"error getting info of tree item searching for parents: %I32d", GetLastError());
+{WCHAR msg[64];
+wsprintf(msg,L"%p %p",tvi.lParam,parent);
+MessageBoxW(NULL,msg,NULL,MB_OK);
+}
+		if ((HWND) (tvi.lParam) == parent)
 			return cur;
-		child = (HTREEITEM) SendMessageW(tree, TVM_GETNEXTITEM, TVGN_CHILD, (LPARAM) cur);
-		if (child != NULL) {
-			child = locate(tree, lParam, child);
-			if (child != NULL)
-				return child;
-		}
 		cur = (HTREEITEM) SendMessageW(tree, TVM_GETNEXTITEM, TVGN_NEXT, (LPARAM) cur);
 	}
+	panic(L"parent window not found in tree view");
 	return NULL;			// make compiler happy
-}
-
-static HTREEITEM windowParentNode(HWND tree, HWND hwnd)
-{
-	DWORD style;
-	HTREEITEM item;
-
-	// TODO replace this with the explicit call
-	style = GetWindowStyle(hwnd);
-	// this function only works on child windows as their parents already have items
-	// all non-child windows are at the top level of our tree, so...
-	if ((style & WS_CHILD) == 0)
-		return TVI_ROOT;
-
-	item = (HTREEITEM) SendMessageW(tree, TVM_GETNEXTITEM, TVGN_ROOT, 0);
-	item = (HTREEITEM) SendMessageW(tree, TVM_GETNEXTITEM, TVGN_CHILD, (LPARAM) item);
-	item = locate(tree, (LPARAM) hwnd, item);
-	if (item == NULL)
-		panic(L"parent window not found in tree view");
-	return item;
 }
 
 struct enumTree {
