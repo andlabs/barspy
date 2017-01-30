3bb263f3a8b3497486f34f0dde72c0fda740263e
Pietro Gagliardi <pietro10@mac.com>
2017-01-30 18:29:53 -0500

    Fixed EnumChildWindows() multi-recursion once and for all.

--- enum.cpp
+++ enum.cpp
@@ -4,28 +4,63 @@
 struct enumTree {
 	HWND treeview;
 	HTREEITEM (*f)(HWND treeview, HWND window, HTREEITEM parent);
-	HTREEITEM parent;
+	// EnumChildWindows() recurses
+	// fortunately it seems to always recurse in depth-first order
+	// and since it never revisits a node, we can just do this
+	// TODO I guess: make sure it never visits a new child of an old parent
+	std::vector<HWND> *curHWNDPath;
+	std::vector<HTREEITEM> *curNodePath;
 };
 
-// TODO this adds duplicates because EnumChildWindows() already recurses
 static BOOL CALLBACK ewtEach(HWND hwnd, LPARAM lParam)
 {
-	struct enumTree *parent = (struct enumTree *) lParam;
-	struct enumTree child;
+	struct enumTree *t = (struct enumTree *) lParam;
+	HWND parent;
+	HTREEITEM parentNode, newNode;
 
-	child.treeview = parent->treeview;
-	child.f = parent->f;
-	child.parent = (*(parent->f))(parent->treeview, hwnd, parent->parent);
-	EnumChildWindows(hwnd, ewtEach, (LPARAM) (&child));
+	parent = GetAncestor(hwnd, GA_PARENT);
+	if (parent == GetDesktopWindow())
+		parent = NULL;
+
+	if (parent == NULL) {
+		t->curHWNDPath->clear();
+		t->curNodePath->clear();
+		parentNode = TVI_ROOT;
+	} else {
+		while (t->curHWNDPath->size() != 0) {
+			if (t->curHWNDPath->back() == parent)
+				break;
+			t->curHWNDPath->pop_back();
+			t->curNodePath->pop_back();
+		}
+		if (t->curNodePath->size() == 0)
+			// back to root somehow??? TODO
+			parentNode = TVI_ROOT;
+		else
+			parentNode = t->curNodePath->back();
+	}
+
+	newNode = (*(t->f))(t->treeview, hwnd, parentNode);
+	t->curHWNDPath->push_back(hwnd);
+	t->curNodePath->push_back(newNode);
+
 	return TRUE;
 }
 
 void enumWindowTree(HWND treeview, HTREEITEM (*f)(HWND treeview, HWND window, HTREEITEM parent))
 {
-	struct enumTree topmost;
+	struct enumTree t;
 
-	topmost.treeview = treeview;
-	topmost.f = f;
-	topmost.parent = TVI_ROOT;
-	EnumChildWindows(NULL, ewtEach, (LPARAM) (&topmost));
+	t.treeview = treeview;
+	t.f = f;
+	t.curHWNDPath = new std::vector<HWND>;
+	if (t.curHWNDPath->capacity() < 16)
+		t.curHWNDPath->reserve(16);
+	t.curNodePath = new std::vector<HTREEITEM>;
+	if (t.curNodePath->capacity() < 16)
+		t.curNodePath->reserve(16);
+	// passing NULL only does EnuMWindows()
+	EnumChildWindows(GetDesktopWindow(), ewtEach, (LPARAM) (&t));
+	delete t.curNodePath;
+	delete t.curHWNDPath;
 }
