8387381da86814134d18f296b44f046584cf446a
Pietro Gagliardi <pietro10@mac.com>
2017-02-03 19:59:40 -0500

    Added shims for LoadLibraryW().

---
+++ call/loadlibrary/call.cpp
@@ -0,0 +1,36 @@
+// 25 january 2017
+#define UNICODE
+#define _UNICODE
+#define STRICT
+#define STRICT_TYPED_ITEMIDS
+
+// get Windows version right; right now Windows Vista
+// unless otherwise stated, all values from Microsoft's sdkddkver.h
+// TODO is all of this necessary? how is NTDDI_VERSION used?
+// TODO plaform update sp2
+#define WINVER			0x0600	/* from Microsoft's winnls.h */
+#define _WIN32_WINNT		0x0600
+#define _WIN32_WINDOWS	0x0600	/* from Microsoft's pdh.h */
+#define _WIN32_IE			0x0700
+#define NTDDI_VERSION		0x06000000
+
+#include <windows.h>
+
+// This isn't meant to be compiled; rather, its assembly output is copied into the main program.
+
+struct tpargs {
+	HMODULE (WINAPI *LoadLibraryWPtr)(LPCWSTR lpFileName);
+	DWORD (WINAPI *GetLastErrorPtr)(void);
+	LPWSTR name;
+	HMODULE module;
+	DWORD lastError;
+};
+
+DWORD WINAPI ThreadProc(LPVOID lpParameter)
+{
+	struct tpargs *a = (struct tpargs *) lpParameter;
+
+	a->module = (*(a->LoadLibraryWPtr))(a->name);
+	a->lastError = (*(a->GetLastErrorPtr))();
+	return 0;
+}
